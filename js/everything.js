c63cc99a49ba1eea59cebff2d76e022ac17aa19f
Thomas <tpech46@gmail.com>
2015-01-17 12:50:57 -0500

    javascript additions

--- js/everything.js
+++ js/everything.js
@@ -2,15 +2,15 @@ document.getElementById("add-class-button").onclick = function(){
 
 	
 
-	//alert(document.getElementById("class-name").value);
-
-	var className = document.getElementById("class-name").value;
-
-	
+ 	var className = document.getElementById("class-name").value;
 
 	var creditHours = document.getElementById("credit-hours").value;
 
+	//TODO change that ass
+	var startTime = getTime(11, 30, "AM");
+	//var endTime = getTime(1, 0, "PM");
 
+	alert(startTime); 
 
 	alert("yo \n" + className + "\n" + creditHours + "\n");
 
@@ -21,4 +21,17 @@ document.getElementById("optimize-button").onclick = function(){
 
 	alert("hooptimizing...");
 
+}
+
+function getTime(hours, minutes, period) {
+
+	hours += minutes/60;
+
+	period = period.toLowerCase();
+
+	if (period == "pm") {
+		hours += 12;
+	}
+
+    return hours;
 }
