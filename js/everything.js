c7a5d90a1430b6271552eb63ad1902d007624426
Thomas <tpech46@gmail.com>
2015-01-17 14:44:58 -0500

    dfs

--- js/everything.js
+++ js/everything.js
@@ -1,37 +1,34 @@
-document.getElementById("add-class-button").onclick = function(){ 
+document.getElementById("add-class-button").onclick = function(){
 
-	
+  var className = document.getElementById("class-name").value;
 
-	var className = document.getElementById("class-name").value;
+  var creditHours = document.getElementById("credit-hours").value;
 
-	var creditHours = document.getElementById("credit-hours").value;
+  //TODO change that ass
+  var startTime = getTime(11, 30, "AM");
+  var endTime = getTime(1, 0, "PM");
 
-	//TODO change that ass
-	var startTime = getTime(11, 30, "AM");
-	var endTime = getTime(1, 0, "PM");
 
-	//var monday = document.getElementById("M").value;
-	
-	//alert(document.querySelector('.checkbox:checked').value);
+  var inputFields = document.getElementsByTagName('input');
+  var numInputFields = inputFields.length;
 
-	//gets all the input tags in frm, and their number
-	var inputFields = document.getElementsByTagName('input');
-	var numInputFields = inputFields.length;
+  var checkedDays = [];
+  var mustTake = false;
 
-	var checkedDays = [];
-
-  //traverse the inpfields elements, and adds the value of selected (checked) checkbox in selchbox
   for(var i = 0; i < numInputFields; i++) {
 
-  	if(inputFields[i].type == 'checkbox' && inputFields[i].checked == true /*&& inputFields[i].class == 'days*/) {
+	if(inputFields[i].type == 'checkbox' &&
+		inputFields[i].name == 'day-week' &&
+		inputFields[i].checked == true) {
+		checkedDays.push(inputFields[i].value);
+	}
+	else if (inputFields[i].id == 'semester-radio-yes' && inputFields[i].checked){
+		mustTake = true;
+	}
 
-  		alert(inputFields[i].name);
-  		alert("found one");
-  	}
   }
 
-
-  alert("yo \n" + className + "\n" + creditHours + "\n" + startTime + "\n" + endTime);
+  alert("yo \n" + className + "\n" + creditHours + "\n" + startTime + "\n" + endTime + "\n" + mustTake);
 
 
 }
