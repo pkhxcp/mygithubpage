1628b8b29a5564ea7adcfb1ed39b1c790d5b39c0
Thomas <tpech46@gmail.com>
2015-01-17 13:53:31 -0500

    mo java script

--- js/everything.js
+++ js/everything.js
@@ -2,17 +2,36 @@ document.getElementById("add-class-button").onclick = function(){
 
 	
 
- 	var className = document.getElementById("class-name").value;
+	var className = document.getElementById("class-name").value;
 
 	var creditHours = document.getElementById("credit-hours").value;
 
 	//TODO change that ass
 	var startTime = getTime(11, 30, "AM");
-	//var endTime = getTime(1, 0, "PM");
+	var endTime = getTime(1, 0, "PM");
 
-	alert(startTime); 
+	//var monday = document.getElementById("M").value;
+	
+	//alert(document.querySelector('.checkbox:checked').value);
 
-	//alert("yo \n" + className + "\n" + creditHours + "\n");
+	//gets all the input tags in frm, and their number
+	var inputFields = document.getElementsByTagName('input');
+	var numInputFields = inputFields.length;
+
+	var checkedDays = [];
+
+  //traverse the inpfields elements, and adds the value of selected (checked) checkbox in selchbox
+  for(var i = 0; i < numInputFields; i++) {
+
+  	if(inputFields[i].type == 'checkbox' && inputFields[i].checked == true /*&& inputFields[i].class == 'days*/) {
+
+  		alert(inputFields[i].name);
+  		alert("found one");
+  	}
+  }
+
+
+  alert("yo \n" + className + "\n" + creditHours + "\n" + startTime + "\n" + endTime);
 
 
 }
@@ -33,5 +52,5 @@ function getTime(hours, minutes, period) {
 		hours += 12;
 	}
 
-    return hours;
+	return hours;
 }
