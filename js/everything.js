2c194b1f398445118f4150ece3e3dc86ff8c54ed
Thomas <tpech46@gmail.com>
2015-01-17 14:48:47 -0500

    dffs

--- js/everything.js
+++ js/everything.js
@@ -1,53 +1,52 @@
-document.getElementById("add-class-button").onclick = function(){
+document.getElementById("add-class-button").onclick = function() {
 
-  var className = document.getElementById("class-name").value;
+    var className = document.getElementById("class-name").value;
 
-  var creditHours = document.getElementById("credit-hours").value;
+    var creditHours = document.getElementById("credit-hours").value;
 
-  //TODO change that ass
-  var startTime = getTime(11, 30, "AM");
-  var endTime = getTime(1, 0, "PM");
+    //TODO change that ass
+    var startTime = getTime(11, 30, "AM");
+    var endTime = getTime(1, 0, "PM");
 
 
-  var inputFields = document.getElementsByTagName('input');
-  var numInputFields = inputFields.length;
+    var inputFields = document.getElementsByTagName('input');
+    var numInputFields = inputFields.length;
 
-  var checkedDays = [];
-  var mustTake = false;
+    var checkedDays = [];
+    var mustTake = false;
 
-  for(var i = 0; i < numInputFields; i++) {
+    for (var i = 0; i < numInputFields; i++) {
 
-	if(inputFields[i].type == 'checkbox' &&
-		inputFields[i].name == 'day-week' &&
-		inputFields[i].checked == true) {
-		checkedDays.push(inputFields[i].value);
-	}
-	else if (inputFields[i].id == 'semester-radio-yes' && inputFields[i].checked){
-		mustTake = true;
-	}
+        if (inputFields[i].type == 'checkbox' &&
+            inputFields[i].name == 'day-week' &&
+            inputFields[i].checked == true) {
+            checkedDays.push(inputFields[i].value);
+        } else if (inputFields[i].id == 'semester-radio-yes' && inputFields[i].checked) {
+            mustTake = true;
+        }
 
-  }
+    }
 
-  alert("yo \n" + className + "\n" + creditHours + "\n" + startTime + "\n" + endTime + "\n" + mustTake);
+    alert("yo \n" + className + "\n" + creditHours + "\n" + startTime + "\n" + endTime + "\n" + mustTake);
 
 
 }
 
-document.getElementById("optimize-button").onclick = function(){ 
+document.getElementById("optimize-button").onclick = function() {
 
-	alert("hooptimizing...");
+    alert("hooptimizing...");
 
 }
 
 function getTime(hours, minutes, period) {
 
-	hours += minutes/60;
+    hours += minutes / 60;
 
-	period = period.toLowerCase();
+    period = period.toLowerCase();
 
-	if (period == "pm") {
-		hours += 12;
-	}
+    if (period == "pm") {
+        hours += 12;
+    }
 
-	return hours;
+    return hours;
 }
