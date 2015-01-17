274417d712bcbd5700af34ebadc271685748fac1
Thomas <tpech46@gmail.com>
2015-01-17 09:35:16 -0500

    adding everything.js and a couple changes to index.html

---
+++ js/everything.js
@@ -0,0 +1,7 @@
+//document.getElementById("class-modal-button").onclick = document.getElementById("class-modal").modal('show');
+
+//document.getElementById("class-modal-button").onclick =  alert("YOOOO");
+
+document.getElementById("add-class-button").onclick = document.getElementById("class-modal").modal('hide');
+
+//document.getElementById("optimize-button").onclick = document.getElementById("optimize-modal").modal('hide');
